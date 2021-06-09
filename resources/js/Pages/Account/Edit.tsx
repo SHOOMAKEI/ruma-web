f81284ac20478a67ab60baff2262f565f96ed877
omakei <omakei96@gmail.com>
2021-06-09 10:29:55 +0300

    WIP User accounting

---
+++ resources/js/Pages/Account/Edit.tsx
@@ -0,0 +1,162 @@
+import React, {Component} from "react";
+import {InertiaLink, useForm, usePage} from "@inertiajs/inertia-react";
+import route from "ziggy-js";
+import TextInput from "../../Shared/TextInput";
+import LoadingButton from "../../Shared/LoadingButton";
+import AuthFramework from "../Auth/AuthFramework";
+import Layout from "../../Shared/Layout";
+import Index from "./Index";
+import TextAreaInput from "../../Shared/TextAreaInput";
+import SelectInput from "../../Shared/SelectInput";
+import CheckBoxInput from "../../Shared/CheckBoxInput";
+// @ts-ignore
+import FileInput from "../../Shared/FileInput";
+import CardWaper from "../../Shared/CardWaper";
+import {Company} from "../../Shared/Types";
+
+
+function Create() {
+    const {company} = usePage().props
+    let checked: boolean = false;
+    // @ts-ignore
+    const { data, setData, errors, put, processing } = useForm({
+        id: (company as Company).id || '',
+        name: (company as Company).name || '',
+        email: (company as Company).email || '',
+        phone: (company as Company).phone,
+        currency: (company as Company).currency || '',
+        tax_number: (company as Company).tax_number || '',
+        address: (company as Company).address || '',
+        is_active: (company as Company).is_active || (checked),
+        logo: ''
+    });
+
+    function handleSubmit(e: { preventDefault: () => void; }) {
+        e.preventDefault();
+        put(route('companies.update', (company as Company).id));
+    }
+
+    return(
+        <CardWaper>
+                <form onSubmit={handleSubmit} className="form w-100">
+                    <div className="fv-row mb-5 row">
+                        <TextInput
+                            className="mt-10 col-md-6"
+                            label="Name"
+                            placeholder="Name"
+                            name="name"
+                            type="text"
+                            label_required={true}
+                            required
+                            errors={errors.name}
+                            value={data.name}
+                            onChange={(e: { target: { value: string; }; }) => setData('name', e.target.value)}
+                        />
+                        <TextInput
+                            className="mt-10 col-md-6"
+                            label="Email"
+                            placeholder="Email"
+                            name="email"
+                            type="text"
+                            required
+                            label_required={true}
+                            errors={errors.email}
+                            value={data.email}
+                            onChange={(e: { target: { value: string; }; }) => setData('email', e.target.value)}
+                        />
+                    </div>
+                    <div className="fv-row mb-5 row">
+                        <SelectInput
+                            className="mt-10 col-md-6"
+                            label="Currency"
+                            placeholder="Currency"
+                            name="currency"
+                            type="text"
+                            required
+                            label_required={true}
+                            errors={errors.currency}
+                            value={data.currency}
+                            onChange={(e: { target: { value: string; }; }) => setData('currency', e.target.value)}
+                        >
+                            <option value="USD"> USD</option>
+                            <option value="NGN"> NGN</option>
+                            <option value="TZS"> TZS</option>
+                            </SelectInput>
+                        <TextInput
+                            className="mt-10 col-md-6"
+                            label="Tax Number"
+                            placeholder="Tax Number"
+                            name="tax_number"
+                            type="text"
+                            required
+                            label_required={true}
+                            errors={errors.tax_number}
+                            value={data.tax_number}
+                            onChange={(e: { target: { value: string; }; }) => setData('tax_number', e.target.value)}
+                        />
+                    </div>
+                    <div className="fv-row mb-5 row">
+                    <TextInput
+                        className="mt-10 col-md-6"
+                        label="Phone"
+                        placeholder="Phone"
+                        name="phone"
+                        type="text"
+                        required
+                        label_required={true}
+                        errors={errors.phone}
+                        value={data.phone}
+                        onChange={(e: { target: { value: string; }; }) => setData('phone', e.target.value)}
+                    />
+                    <TextAreaInput
+                        className="mt-10 col-md-6"
+                        label="Address"
+                        placeholder="Address"
+                        name="address"
+                        required
+                        label_required={true}
+                        errors={errors.address}
+                        value={data.address}
+                        onChange={(e: { target: { value: string; }; }) => setData('address', e.target.value)}
+                    />
+                </div>
+                <div className="fv-row mb-5 row">
+                    <FileInput
+                        className="mt-10 col-md-6"
+                        name="logo"
+                        label="Logo"
+                        accept="image/*"
+                        required
+                        label_required={true}
+                        errors={errors.logo}
+                        value={data.logo}
+                        callback={setData}
+                    />
+                    <CheckBoxInput
+                        className="mt-10 col-md-6"
+                        label="Is Active"
+                        name="is_active"
+                        required
+                        label_required={true}
+                        errors={errors.is_active}
+                        value={data.is_active}
+                        onChange={(e: { target: { checked: boolean; }; }) => setData('is_active', e.target.checked)}
+                    />
+                </div>
+                    <div className="fv-row">
+                        <LoadingButton
+                            type="submit"
+                            loading={processing} >
+                            Save
+                        </LoadingButton>
+                    </div>
+                </form>
+        </CardWaper>
+    )
+}
+
+Create.layout = (page: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined) => <Layout
+    children={page}  title="Create Company"
+ />;
+
+export default Create;
--- resources/js/Shared/FileInput.jsx
+++ resources/js/Shared/FileInput.jsx
@@ -20,8 +20,9 @@ export default ({ className, name, label, accept, errors, label_required, callba
       let base64: string | ArrayBuffer | null | undefined;
       reader.onload =  function(event) {
           base64 = event.target?.result
+
           // @ts-ignore
-          setFile(base64 );
+          setFile( base64 );
           callback(name,base64);
           console.log(base64)
       }
@@ -37,7 +38,7 @@ export default ({ className, name, label, accept, errors, label_required, callba
         <input
           id={name}
           name={name}
-          ref={ () => fileInput}
+          ref={fileInput}
           accept={accept}
           type="file"
           data-toggle="custom-file-input"
