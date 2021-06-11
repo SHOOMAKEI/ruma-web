b6e96bd33cc555133f5bba42708ed149e8f58cc7
Michael Omakei <omakei96@gmail.com>
2021-06-12 02:21:14 +0300

    Update RolesAndPermissionsSeeder.php

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -17,68 +17,78 @@ class RolesAndPermissionsSeeder extends Seeder
     {
         // Reset cached roles and permissions
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+//        Permission::truncffate();
 
-        // create permissions
-        Permission::findOrCreate('user.update');
-        Permission::findOrCreate('user.delete');
-        Permission::findOrCreate('user.read');
-        Permission::findOrCreate('user.create');
+        foreach (config('auth.guards') as $guard=>$value) {
+//            dd($guard);
+            // create permissions
+            Permission::findOrCreate('user.update',$guard);
+            Permission::findOrCreate('user.delete',$guard);
+            Permission::findOrCreate('user.read',$guard);
+            Permission::findOrCreate('user.create',$guard);
 
-        Permission::findOrCreate('company.update');
-        Permission::findOrCreate('company.delete');
-        Permission::findOrCreate('company.read');
-        Permission::findOrCreate('company.create');
+            Permission::findOrCreate('company.update',$guard);
+            Permission::findOrCreate('company.delete',$guard);
+            Permission::findOrCreate('company.read',$guard);
+            Permission::findOrCreate('company.create',$guard);
 
-        Permission::findOrCreate('role.update');
-        Permission::findOrCreate('role.delete');
-        Permission::findOrCreate('role.read');
-        Permission::findOrCreate('role.create');
+            Permission::findOrCreate('role.update',$guard);
+            Permission::findOrCreate('role.delete',$guard);
+            Permission::findOrCreate('role.read',$guard);
+            Permission::findOrCreate('role.create',$guard);
 
-        Permission::findOrCreate('permission.update');
-        Permission::findOrCreate('permission.delete');
-        Permission::findOrCreate('permission.read');
-        Permission::findOrCreate('permission.create');
+            Permission::findOrCreate('permission.update',$guard);
+            Permission::findOrCreate('permission.delete',$guard);
+            Permission::findOrCreate('permission.read',$guard);
+            Permission::findOrCreate('permission.create',$guard);
 
-        Permission::findOrCreate('geopolitical_zone.update');
-        Permission::findOrCreate('geopolitical_zone.delete');
-        Permission::findOrCreate('geopolitical_zone.read');
-        Permission::findOrCreate('geopolitical_zone.create');
+            Permission::findOrCreate('geopolitical_zone.update',$guard);
+            Permission::findOrCreate('geopolitical_zone.delete',$guard);
+            Permission::findOrCreate('geopolitical_zone.read',$guard);
+            Permission::findOrCreate('geopolitical_zone.create',$guard);
 
-        Permission::findOrCreate('region.update');
-        Permission::findOrCreate('region.delete');
-        Permission::findOrCreate('region.read');
-        Permission::findOrCreate('region.create');
+            Permission::findOrCreate('region.update',$guard);
+            Permission::findOrCreate('region.delete',$guard);
+            Permission::findOrCreate('region.read',$guard);
+            Permission::findOrCreate('region.create',$guard);
 
-        Permission::findOrCreate('district.update');
-        Permission::findOrCreate('district.delete');
-        Permission::findOrCreate('district.read');
-        Permission::findOrCreate('district.create');
+            Permission::findOrCreate('district.update',$guard);
+            Permission::findOrCreate('district.delete',$guard);
+            Permission::findOrCreate('district.read',$guard);
+            Permission::findOrCreate('district.create',$guard);
 
-        Permission::findOrCreate('shop.update');
-        Permission::findOrCreate('shop.delete');
-        Permission::findOrCreate('shop.read');
-        Permission::findOrCreate('shop.create');
+            Permission::findOrCreate('shop.update',$guard);
+            Permission::findOrCreate('shop.delete',$guard);
+            Permission::findOrCreate('shop.read',$guard);
+            Permission::findOrCreate('shop.create',$guard);
 
-        Permission::findOrCreate('sale_zone.update');
-        Permission::findOrCreate('sale_zone.delete');
-        Permission::findOrCreate('sale_zone.read');
-        Permission::findOrCreate('sale_zone.create');
+            Permission::findOrCreate('sale_zone.update',$guard);
+            Permission::findOrCreate('sale_zone.delete',$guard);
+            Permission::findOrCreate('sale_zone.read',$guard);
+            Permission::findOrCreate('sale_zone.create',$guard);
 
-        Permission::findOrCreate('country.update');
-        Permission::findOrCreate('country.delete');
-        Permission::findOrCreate('country.read');
-        Permission::findOrCreate('country.create');
+            Permission::findOrCreate('country.update',$guard);
+            Permission::findOrCreate('country.delete',$guard);
+            Permission::findOrCreate('country.read',$guard);
+            Permission::findOrCreate('country.create',$guard);
 
-        Permission::findOrCreate('employee.update');
-        Permission::findOrCreate('employee.delete');
-        Permission::findOrCreate('employee.read');
-        Permission::findOrCreate('employee.create');
+            Permission::findOrCreate('employee.update',$guard);
+            Permission::findOrCreate('employee.delete',$guard);
+            Permission::findOrCreate('employee.read',$guard);
+            Permission::findOrCreate('employee.create',$guard);
 
-        Role::findOrCreate('product-manager')->givePermissionTo('user.read');
-        Role::findOrCreate('data-analyst')->givePermissionTo('user.read');
-        Role::findOrCreate('sales-manager')->givePermissionTo('user.read');
-        Role::findOrCreate('sales-representative')->givePermissionTo('user.read');
-        Role::findOrCreate('supervisor')->givePermissionTo('user.read');
-        Role::findOrCreate('system-administrator')->givePermissionTo(Permission::all());
+            Role::findOrCreate('product-manager',$guard)->givePermissionTo('user.read');
+            Role::findOrCreate('data-analyst',$guard)->givePermissionTo('user.read');
+            Role::findOrCreate('sales-manager',$guard)->givePermissionTo('user.read');
+            Role::findOrCreate('sales-representative',$guard)->givePermissionTo('user.read');
+            Role::findOrCreate('supervisor',$guard)->givePermissionTo('user.read');
+            Role::findOrCreate('system-administrator',$guard);
+
+        }
+        $role_web = Role::findByName('system-administrator', 'web');
+        $role_web->givePermissionTo(Permission::where('guard_name', 'web')->get());
+        $role_sanctum = Role::findByName('system-administrator', 'sanctum');
+        $role_sanctum->givePermissionTo(Permission::where('guard_name', 'sanctum')->get());
+
     }
 }
